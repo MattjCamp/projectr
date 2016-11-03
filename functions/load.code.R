91296ecf05b43c32a5fc80e6f137f6459f323a0c
Matthew Campbell <mjcampbell74@gmail.com>
2016-11-03 13:08:23 -0400

    Removed some libraries

--- functions/load.code.R
+++ functions/load.code.R
@@ -4,10 +4,8 @@ options(stringsAsFactors = FALSE)
 
 library(dplyr)
 library(stringr)
-library(sqldf)
 library(datapointsr)
 library(coderr)
-library(overridesr)
 library(cohortr)
 
 source(sprintf("%s/functions/functions.R",
