38b5b27fb8c55f93ed8e7138525289593b5bc2ea
mattjcamp <mjcampbell74@gmail.com>
2017-01-30 13:06:04 -0500

    Built CB version

--- functions/load.code.R
+++ functions/load.code.R
@@ -1,15 +1,5 @@
 
-options(scipen = 999)
-options(stringsAsFactors = FALSE)
-
-library(dplyr)
-library(readr)
-library(readxl)
-library(stringr)
-library(dbr)
-library(datapointsr)
-library(coderr)
-library(cohortr)
+matt::load_libraries()
 
 source(sprintf("%s/functions/functions.R",
                dirname(getwd())))
