41ff6fe73cb62b352a5fcfbc8865088040102f19
mattjcamp <mjcampbell74@gmail.com>
2017-02-17 08:07:56 -0500

    Updated gitignore

--- functions/load.code.R
+++ functions/load.code.R
@@ -1,5 +1,18 @@
 
-matt::load_libraries()
+options(scipen = 999)
+options(stringsAsFactors = FALSE)
+
+library(dplyr)
+library(readr)
+library(readxl)
+library(stringr)
+library(datapointsr)
+library(coderr)
+library(cohortr)
+library(cbcohortr)
+library(dbr)
+
+conn <- cornerstone_connection()
 
 source(sprintf("%s/functions/functions.R",
                dirname(getwd())))
